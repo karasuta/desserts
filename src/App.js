d59a5b82ba88b9247f743552d6c4361b026992e7
karasuta <kargina.ts@gmail.com>
2022-10-16 20:51:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const[desserts, setDesserts] = useState (data);
+  const removeDessert=(id)=>{
+    let newDesserts = desserts.filter(element=>element.id!==id);
+    setDesserts(newDesserts);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div>
+        <h1>ТОП-{desserts.length} самых вкусных десертов мира, которых можно приготовить дома</h1> 
+      </div>
+      {desserts.map((item=>{
+        const{id,name, country,picture,definition}=item;
+        return(<div key={id} className='dessert_container'>
+          <div className='title_container'>
+            <div className='dessert_name'>
+              <h2>{name}</h2>
+            </div>
+            <div className='dessert_country'>
+              <h2>{country}</h2>
+            </div>
+          </div>
+          
+          <div className='container'>
+            <img src={picture} width='500px' alt="dessert"/>
+          </div>
+          <div>
+            <p>{definition}</p>
+          </div>
+          <div className='btn_container'>
+            <button className='btn' onClick={()=>removeDessert(id)}>удалить</button>
+          </div>
+        </div>
+        )
+      }))}
+    <div className='bnt_delete_container'>
+      <button className='btn_delete' onClick={()=>setDesserts([])}>Удалить все</button>
+    </div>
     </div>
   );
 }
